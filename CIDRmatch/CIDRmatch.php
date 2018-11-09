767b6ad12c65a752e1eb395320aaaab036e6e8c9
Thomas Lutz <thomaslutz.de@gmail.com>
2018-11-09 03:10:25 +0100

    Fix issue #2 and #3

--- CIDRmatch/CIDRmatch.php
+++ CIDRmatch/CIDRmatch.php
@@ -19,8 +19,11 @@ class CIDRmatch
     public function match($ip, $cidr)
     {
         $c = explode('/', $cidr);
-				$subnet = isset($c[0]) ? $c[0] : NULL;
-				$mask   = isset($c[1]) ? $c[1] : NULL;
+        $subnet = isset($c[0]) ? $c[0] : NULL;
+        $mask   = isset($c[1]) ? $c[1] : NULL;
+        if ($mask === null) {
+            $mask = 32;
+        }
 
         if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             // it's valid
@@ -89,11 +92,12 @@ class CIDRmatch
         if (!filter_var($subnetAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) || $subnetMask === NULL || $subnetMask === "" || $subnetMask < 0 || $subnetMask > 32) {
             return false;
         }
-        if ((ip2long($address) & ~((1 << (32 - $subnetMask)) - 1)) == ip2long($subnetAddress)) {
-            return true;
-        }
 
-        return false;
+        $address = ip2long($address);
+        $subnetAddress = ip2long($subnetAddress);
+        $mask = -1 << (32 - $subnetMask);
+        $subnetAddress &= $mask; # nb: in case the supplied subnet wasn't correctly aligned
+        return ($address & $mask) == $subnetAddress;
     }
 
 }
--- tests/CIDRmatchTest/CIDRmatchTest.php
+++ tests/CIDRmatchTest/CIDRmatchTest.php
@@ -56,5 +56,39 @@ class CIDRmatchTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($cidrMatch->match('2001:0db8:85a3:08d3:1319:8a2e:0370:7347', '2001:0db8:85a3:08d3::'));
     }
 
+    /**
+     * Ensure that case from issue #2 (https://github.com/tholu/php-cidr-match/issues/2) works
+     */
+    public function testIssue2() {
+        $cidrMatch = new CIDRmatch();
+
+        $addr = '192.168.1.1';
+        $this->assertTrue($cidrMatch->match($addr, '192.168.1.0/24'));
+        $this->assertFalse($cidrMatch->match($addr, '1.2.3.4/1'));
+        $this->assertFalse($cidrMatch->match($addr, '192.168.1.1/33')); // invalid subnet
+        $this->assertTrue($cidrMatch->match($addr,  '0.0.0.0/0'));
+        $this->assertFalse($cidrMatch->match($addr, '256.256.256/0')); // invalid CIDR notation
+
+        $addr = '10.5.5.1';
+        $this->assertTrue($cidrMatch->match($addr, '10.0.0.1/8'));
+        $this->assertTrue($cidrMatch->match($addr, '10.0.0.10/8'));
+        $this->assertTrue($cidrMatch->match($addr, '10.5.5.0/16'));
+        $this->assertFalse($cidrMatch->match($addr, '10.4.5.0/16'));
+        $this->assertTrue($cidrMatch->match($addr, '10.4.5.0/15'));
+
+        $this->assertTrue($cidrMatch->match('2a01:198:603:0:396e:4789:8e99:890f', '2a01:198:603:0::/65'));
+        $this->assertFalse($cidrMatch->match('2a00:198:603:0:396e:4789:8e99:890f', '2a01:198:603:0::/65'));
+        $this->assertFalse($cidrMatch->match('2a01:198:603:0:396e:4789:8e99:890f', '::1'));
+        $this->assertFalse($cidrMatch->match('0:0:603:0:396e:4789:8e99:0001', '::1'));
+        $this->assertFalse($cidrMatch->match('}__test|O:21:&quot;JDatabaseDriverMysqli&quot;:3:{s:2', '::1'));   
+    }
+
+    /**
+     * Ensure that case from issue #3 (https://github.com/tholu/php-cidr-match/issues/3) works
+     */
+    public function testIssue3() {
+        $cidrMatch = new CIDRmatch();
+        $this->assertFalse($cidrMatch->match('91.124.36.116', '2a02:6b8:0:1a00::/56'));
+    }
 
 }
